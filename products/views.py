046478c0b875b62ae4dd5ffe0f9982b6f7f59a84
Giorgi <giorgitchikadze200@gmail.com>
2025-02-19 21:53:46 +0400

    removed unnecssary imports

--- products/views.py
+++ products/views.py
@@ -1,6 +1,3 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin, DestroyModelMixin
